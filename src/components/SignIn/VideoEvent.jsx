4e1ee815701060d521a389cf5f566fb9de35fed8
unknown <kayv@hotmail.fr>
2023-10-21 22:55:07 +0200

    .

--- src/components/SignIn/VideoEvent.jsx
+++ src/components/SignIn/VideoEvent.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 import '../../styles/SignIn/video.css'
+import Seraphine from '../../media/SeraphineEvent.mp4';
 
 export default function VideoEvent(video) {
 	return (
 		<section className="videoEvent">
-			<video className='video' autoPlay={true} loop muted>
-				<source src={video} />
+			<video className="video" autoPlay={true} loop muted>
+				<source src={Seraphine} />
 			</video>
 		</section>
 	);
--- src/page/SignIn.jsx
+++ src/page/SignIn.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import AsideSign from "../components/SignIn/AsideSign";
 import VideoEvent from "../components/SignIn/VideoEvent";
 import '../styles/SignIn/index.css'
-import Seraphine from '../media/SeraphineEvent.mp4';
-export default function SignIn(){
 
+export default function SignIn(){
     return (
 			<div className="signIn">
 				<AsideSign />
-				<VideoEvent video={Seraphine} />
+				<VideoEvent />
 			</div>
 		);
 }

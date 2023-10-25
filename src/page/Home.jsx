64280b6e226a9e2f6915388c6c23c31320778615
unknown <kayv@hotmail.fr>
2023-10-25 13:45:16 +0200

    .

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Header from "../components/Home/Header";
+import '../styles/Client/header.css'
 
 export default function Home(){
-
     return(
-        <div></div>
+        <div className='home'>
+            <Header />
+        </div>
     )
 }

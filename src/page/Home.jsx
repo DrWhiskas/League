890241cbddefe3bb2cbf21cb678b0ae249ea3463
unknown <kayv@hotmail.fr>
2023-11-01 20:53:54 +0100

    .

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Home/Header";
-import '../styles/Client/header.css'
+import '../styles/Home/home.css'
 
 export default function Home(){
     return(
